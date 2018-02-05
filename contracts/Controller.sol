72382cd112e0389341e54a546fc47eb6ffc49728
bkcninja <workwithmayank@gmail.com>
2018-02-05 13:41:13 +0530

    Added basic ERC20 functions

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -45,4 +45,56 @@ contract Controller is ERC20, Pausable {
   function allowance(address _owner, address _spender) public constant returns (uint256) {
     return allowed[_owner][_spender];
   }
+
+  function approve(address _spender, uint256 _amount) public {
+    require(msg.sender != _spender);
+    allowed[msg.sender][_spender] = _amount;
+  }
+
+  function transfer(address _to, uint256 _amount, bytes _data) public returns (bool) {
+    _transfer(msg.sender, _to, _amount, _data);
+    Transfer(msg.sender, _to, _amount);
+    _checkDestination(msg.sender, _to, _amount, _data);
+    return true;
+  }
+
+  function transfer(address _to, uint256 _amount) public returns (bool) {
+    bytes memory empty;
+    return transfer(_to, _amount, empty);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _amount, bytes _data) public returns (bool) {
+    allowed[_from][_to] = allowed[_from][_to].sub(_amount);
+    _transfer(_from, _to, _amount, _data);
+    Transfer(_from, _to, _amount);
+    _checkDestination(_from, _to, _amount, _data);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _amount) public returns (bool) {
+    bytes memory empty;
+    return transferFrom(_from, _to, _amount, empty);
+  }
+
+  function _transfer(address _from, address _to, uint256 _amount, bytes _data) internal {
+    require(_to != address(this));
+    require(_to != address(0));
+    require(_amount > 0);
+    require(_from != _to);
+    balances[_from] = balances[_from].sub(_amount);
+    balances[_to] = balances[_to].add(_amount);
+  }
+
+  function _checkDestination(address _from, address _to, uint256 _value, bytes _data) internal {
+    // erc223: Retrieve the size of the code on target address, this needs assembly .
+    uint256 codeLength;
+    assembly {
+      codeLength := extcodesize(_to)
+    }
+    if(codeLength>0) {
+      ERC223ReceivingContract untrustedReceiver = ERC223ReceivingContract(_to);
+      // untrusted contract call
+      untrustedReceiver.tokenFallback(_from, _value, _data);
+    }
+  }
 }
