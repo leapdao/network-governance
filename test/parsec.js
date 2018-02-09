a388a33b8e3cf193fba5747800f6b98f8938e78c
bkcninja <workwithmayank@gmail.com>
2018-02-09 15:21:09 +0530

    Test cases and helpers

--- test/parsec.js
+++ test/parsec.js
@@ -1,38 +1,71 @@
-const Parsec = artifacts.require('./Parsec.sol');
 const Controller = artifacts.require('./Controller.sol');
-const hexToString = require('./helpers/hexToString');
+const Parsec = artifacts.require('./Parsec.sol');
+const Founder = web3.eth.accounts[0];
+const Investor = web3.eth.accounts[1];
+const assertJump = require("./helpers/assertJump");
 
-contract('Parsec', (accounts) => {
-  let parsec;
+contract('Test', (accounts) => {
+  let proxy;
+  let proxyInstance;
   let controller;
 
-  beforeEach(async () => {
-    parsec = await Parsec.new();
-    controller = await Controller.new(parsec.address);
-    await parsec.setController(controller.address);
+  before(async () => {
+    controller = await Controller.new();
+    proxyInstance = await Parsec.new();
+    await proxyInstance.setKeyHolder('controller', controller.address);
+    proxy = Controller.at(proxyInstance.address);
+    await proxy.assignTokens(Founder, 4000000000);
+  });
+
+  it('it should delegate call to controller to read balance data', async () => {
+    assert.equal((await proxy.balanceOf(Founder)).toNumber(), 4000000000);
+    assert.equal((await proxy.totalSupply()).toNumber(), 4000000000);
+    assert.equal(await proxyInstance.crate.call('controller'), controller.address)
   });
 
   it('it should delegate call to controller and allow transfer', async () => {
+    await proxy.transfer(Investor, 2000000000)
+    assert.equal((await proxy.balanceOf(Investor)).toNumber(), 2000000000);
+    assert.equal((await proxy.totalSupply()).toNumber(), 4000000000);
+    assert.equal(await proxyInstance.crate.call('controller'), controller.address)
+  });
 
-    var pauseData = ((web3.eth.contract(controller.abi)).at(controller.address)).pause.getData();
-    await web3.eth.sendTransaction({from: accounts[0], to: parsec.address, data: pauseData, gas: 4500000});
+  it('it should delegate call to controller and allow approve plus transferFrom', async () => {
+    await proxy.approve(Investor, 2000000000);
+    assert.equal((await proxy.allowance.call(Founder, Investor)).toNumber(), 2000000000)
 
-    var assignData = ((web3.eth.contract(controller.abi)).at(controller.address)).assignTokens.getData(accounts[0], 10000000000000);
-    await web3.eth.sendTransaction({from: accounts[0], to: parsec.address, data: assignData, gas: 4500000});
+    var transferFrom = ((web3.eth.contract(controller.abi)).at(controller.address)).transferFrom.getData(Founder, Investor, 2000000000, "");
+    await web3.eth.sendTransaction({from: accounts[0], to: proxy.address, data: transferFrom, gas: 4500000});
+    assert.equal((await proxy.balanceOf(Investor)).toNumber(), 4000000000);
+    assert.equal((await proxy.totalSupply()).toNumber(), 4000000000);
+    assert.equal(await proxyInstance.crate.call('controller'), controller.address)
+  });
 
-    var unpauseData = ((web3.eth.contract(controller.abi)).at(controller.address)).unpause.getData();
-    await web3.eth.sendTransaction({from: accounts[0], to: parsec.address, data: unpauseData, gas: 4500000});
+  it('it should allow to change controller and retain data', async () => {
+    await proxy.assignTokens(Founder, 4000000000);
+    await proxy.approve(Investor, 2000000000);
+    var transferFrom = ((web3.eth.contract(controller.abi)).at(controller.address)).transferFrom.getData(Founder, Investor, 2000000000, "");
+    await web3.eth.sendTransaction({from: accounts[0], to: proxy.address, data: transferFrom, gas: 4500000});
 
-    var transferData = ((web3.eth.contract(controller.abi)).at(controller.address)).transfer.getData(accounts[1], 10000000000000);
-    await web3.eth.sendTransaction({from: accounts[0], to: parsec.address, data: transferData, gas: 4500000});
+    // change controller and set
+    const controllerNew = await Controller.new();
+    await controller.kill(controllerNew.address)
+    await proxyInstance.setKeyHolder('controller', controllerNew.address);
 
-    var balanceOfData = ((web3.eth.contract(controller.abi)).at(controller.address)).balanceOf.getData(accounts[1]);
-    const balance = parseInt(await web3.eth.call({from: accounts[1], to: parsec.address, data: balanceOfData, gas: 4500000}), 16)
+    controller = controllerNew;
+    assert.equal((await proxy.balanceOf(Investor)).toNumber(), 6000000000);
+    assert.equal((await proxy.totalSupply()).toNumber(), 8000000000);
+    assert.equal(await proxyInstance.crate.call('controller'), controller.address)
+    assert.equal(await proxyInstance.crate.call('owner'), accounts[0])
+  });
 
-    var totalSupplyData = ((web3.eth.contract(controller.abi)).at(controller.address)).balanceOf.getData(accounts[1]);
-    const totalSupply = parseInt(await web3.eth.call({from: accounts[1], to: parsec.address, data: totalSupplyData, gas: 4500000}), 16);
-
-    assert.equal(balance, 10000000000000);
-    assert.equal(totalSupply, 10000000000000);
+  it('it should not allow to kill controller by non owner', async () => {
+    const controllerNew = await Controller.new();
+    try {
+      await controller.kill(controllerNew.address, {from: Investor});
+      assert.fail("should have failed before")
+    } catch(error) {
+      assertJump(error);
+    }
   });
 });
