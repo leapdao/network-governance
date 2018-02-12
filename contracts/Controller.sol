233bee6f188ada7492582769fe7f520d40394556
bkcninja <workwithmayank@gmail.com>
2018-02-12 17:51:48 +0530

    Added cap logic to token

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -1,12 +1,24 @@
 pragma solidity ^0.4.11;
 
-import "./MintableToken.sol";
+import "./CappedToken.sol";
 
 
-contract Controller is MintableToken {
+contract Controller is CappedToken {
 
   string public name = "ParSeC token";
   string public symbol = "PSC";
   uint8 public decimals = 12;
 
+  function Controller(uint256 _cap) public
+    CappedToken(_cap)
+  {
+
+  }
+
+  function initialize(uint256 _cap) public {
+    require(cap == 0);
+    require(_cap > 0);
+    cap = _cap;
+    totalSupply_ = 0;
+  }
 }
