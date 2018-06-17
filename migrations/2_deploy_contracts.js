2f7816579ec12443e3fdc72feed60feab200fa40
Johann Barbie <johannbarbie@me.com>
2018-06-17 19:48:40 +0200

    add truffle deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,18 @@
-module.exports = function(deployer) {
+const Parsec = artifacts.require('./Parsec.sol');
+const Controller = artifacts.require('./Controller.sol');
 
+module.exports = async function(deployer) {
+  // deploy proxy
+  await deployer.deploy(Parsec);
+  const parsecProxy = await Parsec.deployed();
+  // deploy controller
+  await deployer.deploy(Controller);
+  const controller = await Controller.deployed();
+  // create binding of proxy with controller interface
+  let parsec = Controller.at(parsecProxy.address);
+  // use binding
+  await parsec.initialize(controller.address, 400000000);
+  // check result
+  let cap = await parsec.cap();
+  console.log(cap.toNumber());
 };
--- truffle.js
+++ truffle.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 9000,
+      port: 8545,
       gas: 4500000,
       network_id: "*" // Match any network id
     }
