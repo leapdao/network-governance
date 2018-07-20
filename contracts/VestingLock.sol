6e2a13cb55db871c83f727eec767cf7abc687c1d
Johann Barbie <johannbarbie@me.com>
2018-07-20 16:23:44 +0200

    make vesting use nfts

--- contracts/VestingLock.sol
+++ contracts/VestingLock.sol
@@ -3,38 +3,62 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./income/LatestERC721.sol";
 
 contract VestingLock is Ownable {
     using SafeMath for uint256;
     
     uint256 receivedIncomes;
+    // TODO: think about "holidays" for vesting
     uint256 vestingCliff;   // 44 basic incomes 
     uint256 vestingPeriod; // 176 basic incomes (in 4 years)
     address beneficiary;
+    mapping(uint256 => bool) claims;
     ERC20 token;
+    LatestERC721 nft;
     
-    constructor(ERC20 _token, uint256 _vestingCliff, uint256 _vestingPeriod, address _beneficiary) public {
+    constructor(ERC20 _token, LatestERC721 _nft, uint256 _vestingCliff, uint256 _vestingPeriod, address _beneficiary) public {
         token = _token;
         receivedIncomes = 0;
         vestingCliff = _vestingCliff;
+        owner = _beneficiary;
         beneficiary = _beneficiary;
         vestingPeriod = _vestingPeriod;
+        nft = _nft;
+        claims[0] = true;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return token.balanceOf(this);
+    }
+
+    function balanceOf(address _caller) public view returns (uint256) {
+        uint256 lookaheadIncomes = receivedIncomes;
+        uint256 nftId = nft.latestToken(_caller);
+        if (claims[nftId] == false) {
+            lookaheadIncomes++;
+        }
+        if (lookaheadIncomes >= vestingCliff) {
+            uint256 fraction = (vestingPeriod <= lookaheadIncomes) ? 1 : vestingPeriod.sub(lookaheadIncomes);
+            return totalSupply().div(fraction);
+        }
     }
     
-    function recordBasicIncome() onlyOwner public {
-        require(receivedIncomes < vestingPeriod-1);
+    function transfer(address, uint256 _nftId) onlyOwner public returns (bool) {
+        if (_nftId == 0) {
+            _nftId = nft.latestToken(msg.sender);
+        }
+        require(claims[_nftId] == false);
+        // verify that nft has been issued to beneficiary address
+        uint256 time = uint64(_nftId >> 192);
+        uint256 tacoAmount = uint32(_nftId);
+        uint256 expectedNftId = time << 192 | uint192(uint160(keccak256(abi.encodePacked(beneficiary, tacoAmount, time)))) << 32 | tacoAmount;
+        require(expectedNftId == _nftId);
+        claims[_nftId] = true;
         receivedIncomes++;
-    }
-    
-  modifier onlyBeneficiary() {
-    require(msg.sender == beneficiary);
-    _;
-  }
-    
-    function withdraw() onlyBeneficiary public {
-        require(receivedIncomes >= vestingCliff);
-        uint256 balance = token.balanceOf(this);
-        token.transfer(msg.sender, balance.div(vestingPeriod.sub(receivedIncomes)));
+        if (receivedIncomes >= vestingCliff) {
+            token.transfer(msg.sender, balanceOf(msg.sender));
+        }
     }
 
 }
--- test/vestingLock.js
+++ test/vestingLock.js
@@ -1,48 +1,64 @@
+import chai from 'chai';
+import EVMRevert from './helpers/EVMRevert';
 const VestingLock = artifacts.require('./VestingLock.sol');
 const Parsec = artifacts.require('./Parsec.sol');
+const TacoIncomeToken = artifacts.require('./income/TacoIncomeToken.sol');
 const Controller = artifacts.require('./Controller.sol');
 const { assertRevert } = require('./helpers/assertThrow')
 
+const should = chai
+  .use(require('chai-as-promised'))
+  .should();
+
 contract('VestingLock', (accounts) => {
+  let nft;
   let parsec;
   let parsecProxy;
   let controller;
   const total = 10000000;
+  const alice = accounts[1];
 
   beforeEach(async () => {
-    parsecProxy = await Parsec.new();
-    controller = await Controller.new();
+    nft = await TacoIncomeToken.new().should.be.fulfilled;
+    parsecProxy = await Parsec.new().should.be.fulfilled;
+    controller = await Controller.new().should.be.fulfilled;
     parsec = Controller.at(parsecProxy.address);
     await parsec.initialize(controller.address, 400000000);
-    let result = await parsec.mint(accounts[0], total);
+    let result = await parsec.mint(accounts[0], total).should.be.fulfilled;
   });
 
   it('allow to lock and vest', async () => {
-  	let vesting = await VestingLock.new(parsec.address, 2, 4, accounts[1]);
-  	await parsec.transfer(vesting.address, total);
- 
-    return assertRevert(async () => {
-        await vesting.withdraw({from: accounts[1]});
-    });
-  	await vesting.recordBasicIncome();
-  	// should still fail
-    return assertRevert(async () => {
-        await vesting.withdraw({from: accounts[1]});
-    });
-  	await vesting.recordBasicIncome();
+  	let vesting = await VestingLock.new(parsec.address, nft.address, 2, 4, alice).should.be.fulfilled;
+  	await parsec.transfer(vesting.address, total).should.be.fulfilled;
 
-  	await vesting.withdraw({from: accounts[1]});
-  	let bal = await parsec.balanceOf(accounts[1]);
-  	assert(bal.mul(2).toNumber() === total);
+    // record one income
+    await nft.mint(alice, 1).should.be.fulfilled;
+    await vesting.transfer('', 0, {from: alice }).should.be.fulfilled;
 
-  	await vesting.recordBasicIncome();
+    // check that cliff not passed yet
+    let claimBal = await vesting.balanceOf(alice);
+    assert.equal(claimBal.toNumber(), 0);
 
-    return assertRevert(async () => {
-        await vesting.recordBasicIncome();
-    });
+    // record another income
+    await nft.mint(alice, 2).should.be.fulfilled;
 
-  	await vesting.withdraw({from: accounts[1]});
-  	bal = await parsec.balanceOf(accounts[1]);
+    // check that cliff passed
+    claimBal = await vesting.balanceOf(alice);
+    assert.equal(claimBal.toNumber(), total / 2);
+
+    // claim first tokens
+  	await vesting.transfer('', 0, {from: alice }).should.be.fulfilled;
+  	let bal = await parsec.balanceOf(alice );
+  	assert.equal(bal.mul(2).toNumber(), total);
+
+    // allow claims in different orders
+    const event = await nft.mint(alice, 3).should.be.fulfilled;
+    await nft.mint(alice, 4).should.be.fulfilled;
+    await vesting.transfer('', 0, { from: alice }).should.be.fulfilled;
+    await vesting.transfer('', event.logs[0].args._tokenId, { from: alice }).should.be.fulfilled;
+
+    // check everything claimed
+  	bal = await parsec.balanceOf(alice);
   	assert(bal.toNumber() === total);
   });
 });
